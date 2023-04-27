1d6f63a8ba260680fa6f8adc97e6b6c6a20a37b5
Lucas Gabriel <pintolucas996@gmail.com>
2023-04-27 15:58:56 -0300

    Criação de arquivos base e Inicio da produção da interface de login.

--- App_BancoDigital/App_BancoDigital/MainPage.xaml.cs
+++ App_BancoDigital/App_BancoDigital/MainPage.xaml.cs
@@ -14,5 +14,27 @@ namespace App_BancoDigital
         {
             InitializeComponent();
         }
+
+        private void btn_Entrar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                string email = txt_email.Text;
+                string senha = txt_senha.Text;
+
+                if (PropriedadesApp.list_usuarios.Any(i => (i.Email == email && i.Senha == senha)))
+                {
+                    App.Current.Properties.Add("usuario_logado", email);
+                    App.Current.MainPage = new Protegida();
+                }
+                else
+                    throw new Exception("Dados incorretos, tente novamente.");
+
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Ops!", ex.Message, "OK");
+            }
+        }
     }
 }
---
+++ App_BancoDigital/App_BancoDigital/Model/DadosUsuario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App_BancoDigital.Model
+{
+    public class DadosUsuario
+    {
+
+
+
+    }
+}
