d0793746640d116107da1f20e28f9caa7db4da49
Lucas Gabriel <pintolucas996@gmail.com>
2023-09-12 15:58:33 -0300

    Estilo dos botões

---
+++ App_BancoDigital/App_BancoDigital/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using App_BancoDigital.View;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App_BancoDigital
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : MasterDetailPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(TelaInicial)));
+        }
+
+
+
+    }
+}
