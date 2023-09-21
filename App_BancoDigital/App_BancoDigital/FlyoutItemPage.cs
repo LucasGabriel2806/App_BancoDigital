3b19bc4e4af80ff4c2f7e97e0c63f86804943334
Lucas Gabriel <pintolucas996@gmail.com>
2023-09-21 15:28:26 -0300

    Implementando Flyout

---
+++ App_BancoDigital/App_BancoDigital/FlyoutItemPage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App_BancoDigital
+{
+    class FlyoutItemPage
+    {
+        /**
+         * Detalhes que quero ver na minha página suspensa
+         * 
+         */
+        public string Title { get; set; }
+        public string IconSource { get; set; }
+        public Type TargetPage { get; set; }
+
+
+    }
+}
--- App_BancoDigital/App_BancoDigital/MainPage.xaml.cs
+++ App_BancoDigital/App_BancoDigital/MainPage.xaml.cs
@@ -1,21 +1,60 @@
 ﻿using App_BancoDigital.View;
+using App_BancoDigital.View.Acesso;
 using System;
+using System.ComponentModel;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace App_BancoDigital
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class MainPage : MasterDetailPage
+    [DesignTimeVisible(false)]
+
+    //[XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : FlyoutPage
     {
         public MainPage()
         {
             InitializeComponent();
 
-            //Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(TelaInicial)));
+            Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(TelaInicial)));
         }
 
 
+        private async void btnFazer_Pix_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(View.Pix.EnviarPix)));
+                IsPresented = false;
 
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops!", ex.Message, "OK");
+            }
+        }
+
+        private async void btnReceber_Pix_Clicked(object sender, EventArgs e)
+        {
+
+            try
+            {
+                Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(View.Pix.ReceberPix)));
+                IsPresented = false;
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops!", ex.Message, "OK");
+            }
+
+        }
+
+        private async void BtnSairConta_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Sair", "Deseja sair da sua conta?", "NÃO", "SIM");
+        }
+
+        
     }
 }
